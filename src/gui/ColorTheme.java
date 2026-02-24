b226023cdf6b69799cb227a7bf871f4ac5c47cef
Nachapat Iamphuang <61649121+SeanNachapat@users.noreply.github.com>
2026-02-24 09:40:14 +0700

    Change Primary color

--- src/gui/ColorTheme.java
+++ src/gui/ColorTheme.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public class ColorTheme {
 
       public static final Color BACKGROUND = new Color(20, 25, 45);
-      public static final Color PRIMARY = new Color(0, 200, 255);
+      public static final Color PRIMARY = new Color(255, 255, 0);
       public static final Color BUTTON_BG = new Color(30, 35, 60);
       public static final Color BUTTON_HOVER = new Color(0, 150, 220);
       public static final Color TEXT_COLOR = Color.WHITE;
